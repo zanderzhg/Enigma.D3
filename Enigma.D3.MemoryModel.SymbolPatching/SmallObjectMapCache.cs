905a41071d719274109d42c24bb862af093ee2c9
Enigma32 <enigma32@outlook.com>
2018-03-28 13:26:52 +0200

    Optimize symbol patcher speed

---
+++ Enigma.D3.MemoryModel.SymbolPatching/SmallObjectMapCache.cs
@@ -0,0 +1,60 @@
+﻿using Enigma.D3.MemoryModel.MemoryManagement;
+using Enigma.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.D3.MemoryModel.SymbolPatching
+{
+    internal class SmallObjectMapCache
+    {
+        private readonly HeapNode _node;
+        private readonly Dictionary<int, List<int>> _bins;
+        private readonly byte[] _buffer;
+
+        private SmallObjectMapCache(MemoryManager mm)
+        {
+            _node = mm.LocalHeap.FirstNode.Snapshot();
+            _bins = new Dictionary<int, List<int>>();
+            _buffer = _node.Memory.Reader.ReadBytes(_node.Data, (int)_node.Size);
+
+            var ptr = _node.Data;
+
+            var offset = ptr.ValueAddress - _node.Data.ValueAddress;
+            var end = ptr.ValueAddress + _node.Size - _node.Data.ValueAddress;
+            while (offset < end)
+            {
+                var sf = StructHelper<ulong>.Read(_buffer, offset + SymbolTable.Current.HeapNode.SizeAndFlag);
+                var size = sf >> 1;
+                var used = (sf & 1) == 1;
+
+                if (used)
+                    AddToBins((int)size, offset);
+
+                offset += HeapNode.HeaderSize + (int)size;
+            }
+        }
+
+        public static SmallObjectMapCache Create(MemoryManager mm) => new SmallObjectMapCache(mm);
+
+        public byte[] Buffer => _buffer;
+
+        private void AddToBins(int key, int value)
+        {
+            if (!_bins.TryGetValue(key, out var existing))
+                _bins[key] = existing = new List<int>();
+            existing.Add(value);
+        }
+
+        public List<int> GetBufferOffsets(int size)
+        {
+            if (_bins.TryGetValue(size, out var values))
+                return values;
+            return null;
+        }
+
+        public MemoryAddress GetFullAddress(int offset) => _node.Data.ValueAddress + offset;
+    }
+}
--- Enigma.D3.MemoryModel.SymbolPatching/SymbolPatcher64.cs
+++ Enigma.D3.MemoryModel.SymbolPatching/SymbolPatcher64.cs
@@ -23,37 +23,41 @@ namespace Enigma.D3.MemoryModel.SymbolPatching
             if (mm == null)
                 throw new Exception("E9FF929A-9F44-451F-9209-88AF1F089E21");
 
-            var ractors_block = FindContainerBlock(mm, "RActors");
+            var cache = SmallObjectMapCache.Create(mm);
+
+            var ractors_block = FindContainerBlock(mm, cache, "RActors");
             if (ractors_block == null)
                 throw new Exception("F064FB82-0DE9-4AA2-B50D-034600D64265");
-            
+
             var objmgr_block = mm.LocalHeap.MainBlocks
                 .Where(x => x.IsUsed)
-                .Where(x => x.Size == 0xBA0 || x.Size == 0xBC0) // Korean is 0xBA0 for some reason.
-                .FindBlockThatPointsTo(ractors_block.Data.ValueAddress, out int objmgr_ractors)
-                ??
-                mm.LocalHeap.MainBlocks
-                .Where(x => x.IsUsed)
-                .FindBlockThatPointsTo(ractors_block.Data.ValueAddress, out objmgr_ractors);
+                .FindBlocksOfSize(ObjectManager.SizeOf)
+                .FindBlockThatPointsTo(ractors_block.Data.ValueAddress, out int objmgr_ractors);
             if (objmgr_block == null)
                 throw new Exception("90DEA855-FF3E-4AEB-9D25-59C5C58BC6A4");
 
-            var acds_block = FindContainerBlock(mm, "ActorCommonData");
+            var acds_block = FindContainerBlock(mm, cache, "ActorCommonData");
             if (acds_block == null)
                 throw new Exception("AF1A0A69-8A18-4CB0-9A78-705CEB17DE21");
 
             var acdmgr_block = mm.LocalHeap.MainBlocks
                 .Where(x => x.IsUsed)
+                .FindBlocksOfSize(ACDManager.SizeOf)
                 .FindBlockThatPointsTo(acds_block.Data.ValueAddress, out int acdmgr_acds);
             if (acdmgr_block == null)
                 throw new Exception("040D5BE5-EF81-4253-9FA3-742F1F732C63");
-            
-            var pdm_block = mm.LocalHeap.MainBlocks.Where(x => x.IsUsed).OrderByDescending(x => x.Size)
+
+            var pdm_block = mm.LocalHeap.MainBlocks
+                .Where(x => x.IsUsed)
+                .FindBlocksOfSize(PlayerDataManager.SizeOf)
                 .FirstOrDefault(blk => blk.Data.Cast<Allocator<MemoryObject>>().Dereference().GoodFood == 0x600DF00D);
             if (pdm_block == null)
                 throw new Exception("046B77D0-70C9-436F-990B-0116197DDBCA");
 
-            var player_block = mm.LocalHeap.MainBlocks.Where(x => x.IsUsed).FirstOrDefault(x => x.Size == 0xA3E0);
+            var player_block = mm.LocalHeap.MainBlocks
+                .Where(x => x.IsUsed)
+                .FindBlocksOfSize(Player.SizeOf)
+                .FirstOrDefault();
             if (player_block == null)
                 throw new Exception("30691DA7-AD6A-4B1A-AEDB-BAC863AAA63A");
 
@@ -68,54 +72,20 @@ namespace Enigma.D3.MemoryModel.SymbolPatching
             symbols.Dynamic.Player = player_block.Data.ValueAddress;
         }
 
-        private static HeapNode FindContainerBlock(MemoryManager mm, string name)
+        private static HeapNode FindContainerBlock(MemoryManager mm, SmallObjectMapCache cache, string name)
         {
-            var sb = mm.LocalHeap.First();
-            var sbd = sb.Data.Cast<byte>().ToArray((int)sb.Size);
-            var min_block_size = TypeHelper.SizeOf(typeof(ExpandableContainer<>));
-            var expected_block_size = MemoryObject.AlignedSize(TypeHelper.SizeOf(typeof(ExpandableContainer<>)), 64);
-            var offset = 0;
-            while (offset < sbd.Length)
+            var expected_block_size = MemoryObject.AlignedSize(TypeHelper.SizeOf(typeof(ExpandableContainer<>)), 0x20);
+            var possible_block_size = expected_block_size + 0x20;
+
+            foreach (var size in new[] { expected_block_size, possible_block_size })
             {
-                var node = new BufferMemoryReader(sbd).Read<HeapNode>(offset);
-                if (node.Size == expected_block_size)
+                foreach (var offset in cache.GetBufferOffsets(size))
                 {
-                    if (Encoding.ASCII.GetString(sbd, offset + HeapNode.HeaderSize, name.Length) == name)
-                        return mm.Memory.Reader.Read<HeapNode>(sb.Data.ValueAddress + offset);
+                    if (Encoding.ASCII.GetString(cache.Buffer, offset + HeapNode.HeaderSize, name.Length) == name)
+                        return mm.Memory.Reader.Read<HeapNode>(cache.GetFullAddress(offset));
                 }
-                offset += HeapNode.HeaderSize + (int)node.Size;
-            }
-            //Trace.WriteLine("Plan B: " + name);
-            offset = 0;
-            while (offset < sbd.Length)
-            {
-                var node = new BufferMemoryReader(sbd).Read<HeapNode>(offset);
-                if (node.Size >= min_block_size)
-                {
-                    if (Encoding.ASCII.GetString(sbd, offset + HeapNode.HeaderSize, name.Length) == name)
-                        return mm.Memory.Reader.Read<HeapNode>(sb.Data.ValueAddress + offset);
-                }
-                offset += HeapNode.HeaderSize + (int)node.Size;
             }
             return null;
-        }
-
-        private static IEnumerable<HeapNode> FindContainerBlocks(MemoryManager mm, string name)
-        {
-            var sb = mm.LocalHeap.First();
-            var sbd = sb.Data.Cast<byte>().ToArray((int)sb.Size);
-            var expected_block_size = MemoryObject.AlignedSize(TypeHelper.SizeOf(typeof(ExpandableContainer<>)), 64);
-            var offset = 0;
-            while (offset < sbd.Length)
-            {
-                var node = new BufferMemoryReader(sbd).Read<HeapNode>(offset);
-                if (node.Size == expected_block_size)
-                {
-                    if (Encoding.ASCII.GetString(sbd, offset + HeapNode.HeaderSize, name.Length) == name)
-                        yield return mm.Memory.Reader.Read<HeapNode>(sb.Data.ValueAddress + offset);
-                }
-                offset += HeapNode.HeaderSize + (int)node.Size;
-            }
         }
 
         private static HeapNode FindBlockThatPointsTo(this IEnumerable<HeapNode> blocks, MemoryAddress address, out int offset)
@@ -134,17 +104,15 @@ namespace Enigma.D3.MemoryModel.SymbolPatching
             return default(HeapNode);
         }
 
-        private static IEnumerable<Tuple<HeapNode, int>> FindBlocksThatPointsTo(this IEnumerable<HeapNode> blocks, MemoryAddress address)
+        private static IEnumerable<HeapNode> FindBlocksOfSize(this IEnumerable<HeapNode> blocks, int size)
         {
+            var expected_block_size = MemoryObject.AlignedSize(size, 0x20);
+            var possible_block_size = expected_block_size + 0x20;
+
             foreach (var block in blocks)
             {
-                var aligned = block.Data.Cast<ulong>().ToArray((int)(block.Size / 8));
-                var index = Array.IndexOf(aligned, address);
-                if (index != -1)
-                {
-                    var offset = index * sizeof(ulong);
-                    yield return new Tuple<HeapNode, int>(block, offset);
-                }
+                if (block.Size == expected_block_size || block.Size == possible_block_size)
+                    yield return block;
             }
         }
     }
