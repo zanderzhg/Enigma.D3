154c0ff489f5a8913a6bcaeab31643cbd75796e1
Enigma32 <enigma32@outlook.com>
2018-07-26 18:17:33 +0200

    Add HotbarButton UI control

---
+++ Enigma.D3.MemoryModel/Controls/HotbarButton.cs
@@ -0,0 +1,15 @@
+﻿using Enigma.D3.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.D3.MemoryModel.Controls
+{
+    public class HotbarButton : Control
+    {
+        public bool IsDisabled => Read<int>(0x174C) == 1;
+        public SNO PowerSNO => Read<SNO>(0x1754);
+    }
+}
